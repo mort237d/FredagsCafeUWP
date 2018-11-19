8d155e82f1fb7f56cf932a349810db3aa7d67b01
Morten <morto1995@hotmail.com>
2018-11-19 12:35:57 +0100

    Product and Stock Class are editted

--- FredagsCafeUWP/Models/Product.cs
+++ FredagsCafeUWP/Models/Product.cs
@@ -2,5 +2,45 @@
 {
     class Product
     {
+        #region Fields
+        private double _buyingPrice;
+        private double _sellingPrice;
+        private string _name;
+        private int _amount; 
+        #endregion
+
+        #region Props
+        public double BuyingPrice
+        {
+            get { return _buyingPrice; }
+            set { _buyingPrice = value; }
+        }
+
+        public double SellingPrice
+        {
+            get { return _sellingPrice; }
+            set { _sellingPrice = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        } 
+        #endregion
+
+        public Product(double buyingPrice, double sellingPrice, string name, int amount)
+        {
+            BuyingPrice = buyingPrice;
+            SellingPrice = sellingPrice;
+            Name = name;
+            Amount = amount;
+        }
     }
 }
--- FredagsCafeUWP/Models/Stock.cs
+++ FredagsCafeUWP/Models/Stock.cs
@@ -1,6 +1,24 @@
-﻿namespace FredagsCafeUWP.Models
+﻿using System.Collections.ObjectModel;
+
+namespace FredagsCafeUWP.Models
 {
     class Stock
     {
+        private ObservableCollection<Product> _products;
+        public ObservableCollection<Product> Products
+        {
+            get { return _products; }
+            set { _products = value; }
+        }
+
+        public Stock()
+        {
+            Products = new ObservableCollection<Product>();
+        }
+
+        public void AddProductToOBList(Product product)
+        {
+            Products.Add(product);
+        }
     }
 }
