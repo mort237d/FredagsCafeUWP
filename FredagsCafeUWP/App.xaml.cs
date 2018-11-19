459419e91d04629860fb62b8f5cbca893be5b0c2
Morten <morto1995@hotmail.com>
2018-11-19 13:35:02 +0100

    Startpage er ændret til UserPage

--- FredagsCafeUWP/App.xaml.cs
+++ FredagsCafeUWP/App.xaml.cs
@@ -66,7 +66,7 @@ namespace FredagsCafeUWP
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(UserPage), e.Arguments); //TODO Change back to: rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
