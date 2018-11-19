359f12b7a55bbc7d0087c7c4853579ac38169ac4
LucasvanNiekerk <luca1921@edu.easj.dk>
2018-11-19 12:43:06 +0100

    Added userclass
    Added variables, properties and constructor to User

--- FredagsCafeUWP/Models/User.cs
+++ FredagsCafeUWP/Models/User.cs
@@ -2,5 +2,59 @@
 {
     class User
     {
+        private string _name;
+        private string _grade;
+        private string _education;
+        private string _email;
+        private string _telephoneNumber;
+        private string _userName;
+        private string _password;
+
+        public User(string name, string grade, string education, string email, string telephoneNumber, string userName, string password)
+        {
+            Name = name;
+            Grade = grade;
+            Education = education;
+            Email = email;
+            TelephoneNumber = telephoneNumber;
+            UserName = userName;
+            Password = password;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Grade
+        {
+            get { return _grade; }
+            set { _grade = value; }
+        }
+        public string Education
+        {
+            get { return _education; }
+            set { _education = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public string TelephoneNumber
+        {
+            get { return _telephoneNumber; }
+            set { _telephoneNumber = value; }
+        }
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
     }
 }
