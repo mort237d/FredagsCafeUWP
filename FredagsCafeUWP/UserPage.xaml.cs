7e8765d1c70baefd4ec418f6c58534c14cb96481
Morten <morto1995@hotmail.com>
2018-11-26 20:43:53 +0100

    Updated GUI

--- FredagsCafeUWP/UserPage.xaml.cs
+++ FredagsCafeUWP/UserPage.xaml.cs
@@ -1,26 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.ViewManagement;
-using Windows.UI.Xaml;
+﻿using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace FredagsCafeUWP
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class UserPage : Page
     {
         public UserPage()
@@ -28,8 +10,6 @@ namespace FredagsCafeUWP
             this.InitializeComponent();
 
             var titleBar = ApplicationView.GetForCurrentView().TitleBar;
-
-            // Set active window colors
             titleBar.ForegroundColor = Windows.UI.Colors.White;
             titleBar.BackgroundColor = Windows.UI.Color.FromArgb(1,108,160,220);
             titleBar.ButtonForegroundColor = Windows.UI.Colors.White;
