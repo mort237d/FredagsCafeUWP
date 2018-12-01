3e4a67ab006d0ba30f76663b35d39634385c70d7
Morten <morto1995@hotmail.com>
2018-12-01 07:39:31 +0100

    List of log in and log out is made, but the save/load methods will not do as I want.

--- FredagsCafeUWP/Models/Login/LogOnLogOff.cs
+++ FredagsCafeUWP/Models/Login/LogOnLogOff.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.UserActivities.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using FredagsCafeUWP.Annotations;
@@ -19,7 +21,10 @@ namespace FredagsCafeUWP
         public string PassWord { get; set; }
         private string _wrongLogin;
         private string _wrongLoginColor;
-        private readonly Administration _administration = new Administration();
+        private Administration _administration = new Administration();
+        private List<string> _logInLogOutList;
+
+        private int i = 0;
         #region Props
 
         public string WrongLogin
@@ -42,9 +47,49 @@ namespace FredagsCafeUWP
             }
         }
 
+        public List<string> LogInLogOutList
+        {
+            get { return _logInLogOutList; }
+            set { _logInLogOutList = value; }
+        }
+
+        public Administration Administration
+        {
+            get { return _administration; }
+            set { _administration = value; }
+        }
+
         #endregion
+
+        public LogOnLogOff()
+        {
+            //LoadAsync();
+        }
+
         public void logOffMethod()
         {
+            if (Administration.CurrentUser == null) //TODO Delete when program is done
+            {
+                foreach (var user in Administration.Users)
+                {
+                    if (user.Name == "Morten")
+                    {
+                        Administration.CurrentUser = user;
+                    }
+                }
+            }
+            
+            LogInLogOutList.Add(Administration.CurrentUser.Name + " logged off at " + DateTime.Now.ToString("HH:mm:ss - dd/MM/yyyy"));
+
+            //SaveAsync();
+
+            //i = 0;
+            //foreach (var loginlogoff in LogInLogOutList)
+            //{
+            //    Debug.WriteLine(i + " " + loginlogoff);
+            //    i++;
+            //}
+
             Frame currentFrame = Window.Current.Content as Frame;
             currentFrame.Navigate(typeof(LoginPage));
         }
@@ -52,10 +97,22 @@ namespace FredagsCafeUWP
         public void CheckLogin()
         {
             bool temp = false;
-            foreach (var user in _administration.Users)
+            foreach (var user in Administration.Users)
             {
                 if (user.UserName == UserName && user.Password == PassWord)
                 {
+                    Administration.CurrentUser = user;
+                    LogInLogOutList.Add(UserName + " logged in at " + DateTime.Now.ToString("HH:mm:ss - dd/MM/yyyy"));
+
+                    //SaveAsync();
+
+                    //i = 0;
+                    //foreach (var loginlogoff in LogInLogOutList)
+                    //{
+                    //    Debug.WriteLine(i + " " + loginlogoff);
+                    //    i++;
+                    //}
+
                     temp = true;
                     break;
                 }
@@ -70,6 +127,31 @@ namespace FredagsCafeUWP
                 WrongLogin = "Der er sku noget galt du";
             }
         }
+
+        #region Save/Load
+
+        public async void SaveAsync()
+        {
+            Debug.WriteLine("Saving loginlogout async...");
+            await XmlReadWriteClass.SaveObjectToXml(LogInLogOutList, "loginlogout.xml");
+            Debug.WriteLine("loginlogoutlist.count: " + LogInLogOutList.Count);
+        }
+        private async void LoadAsync()
+        {
+            try
+            {
+                Debug.WriteLine("loading loginlogout async...");
+                LogInLogOutList = await XmlReadWriteClass.ReadObjectFromXmlFileAsync<List<string>>("loginlogout.xml");
+                Debug.WriteLine("loginlogoutlist.count:" + LogInLogOutList.Count);
+            }
+            catch (Exception)
+            {
+                SaveAsync();
+            }
+
+        }
+
+        #endregion
 
         #region INotify
 
